40cfae0a8d1347ee5558dd95c5cdd137021cd14a
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2022-08-12 14:31:44 -0300

    Resolvido novo exercício 09 da lista 02

--- lista-02/js09-seis-por-dois.js
+++ lista-02/js09-seis-por-dois.js
@@ -12,3 +12,36 @@
 // Pŕoxima volta
 
 // Saída com base na próxima folga, calcular dia de trabalho/folga
+
+let dias_semana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+let dataAtual = new Date()
+let dataFinal = (new Date('2022-09-12'))
+let difTempo = Math.abs(dataFinal-dataAtual)
+let tempoNumDia = 1000 * 60 * 60 * 24
+let dias_trabalho = 1
+let dias_folga = 1
+
+let intervalo = 0
+console.log(`Data atual: ${dataAtual.getDate()}/${dataAtual.getMonth()+1}/${dataAtual.getFullYear()}`)
+console.log(`Data final: ${dataFinal.getDate()+1}/${dataFinal.getMonth()+1}/${dataFinal.getFullYear()}`)
+let dia_dinamico = new Date(dataAtual)
+let diaAtualizado = ''
+
+while (intervalo < difTempo) {
+    diaAtualizado = (`Data: ${dia_dinamico.getDate()}/${dia_dinamico.getMonth()+1}/${dia_dinamico.getFullYear()}. Dia da semana: ${dias_semana[dia_dinamico.getDay()]}.`)
+    if (dias_trabalho < 7){
+        console.log(`${diaAtualizado}Trabalha? Sim.`)
+        dias_trabalho++
+    } else {
+        if(dias_folga < 3){
+            console.log(`${diaAtualizado}Trabalha? Não. Folga 0/`)
+            dias_folga++
+        } else {
+            dias_folga = 1
+            dias_trabalho = 1
+        }
+    }
+    intervalo = intervalo + tempoNumDia
+    dia_dinamico = new Date(dia_dinamico)
+    dia_dinamico.setDate(dia_dinamico.getDate() + 1)
+}
